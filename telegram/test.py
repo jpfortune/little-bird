b9ef0cce71330263ed0c86e26b0d30ccdf72e7ac
Jean-Philippe Fortune <07jfortune@gmail.com>
2019-06-19 23:54:43 -0700

    Implement checking against a list of words

--- telegram/test.py
+++ telegram/test.py
@@ -8,39 +8,21 @@ import time
 from bridge_client import BridgeClient
 from telegram_client import TG_Client
 
-WORD_REGEX = re.compile("(\w{3,})+")
-# words = re.findall(WORD_REGEX, msg)
+WORD_REGEX = re.compile("(\w{2,})+")
 
 logging.basicConfig(level=logging.INFO)
 
 
 class Worker:
-    def __init__(self, loop, queue, bridge_client=BridgeClient("bridge")):
+    def __init__(self, loop, queue, bridge_client, check_set=None):
         self._loop = loop
         self._queue = queue
         self.bridge_client = bridge_client
+        self.check_set = check_set
 
     async def run(self):
         # asyncio.ensure_future(self.process_message_queue())
-        while True:
-            try:
-                raw_message = await self._queue.get()
-            except Exception:
-                logging.exception("Error while processing queue")
-
-            logging.info("Parsing: %s" % raw_message)
-
-            # TODO Should we compile the regex for speed gains?
-            # TODO Maybe it's faster if we compiled a regex of everything in
-            # self.check_set
-            words = re.findall(WORD_REGEX, raw_message.message)
-            logging.info("Words Parsed: %s" % words)
-            if words:
-                self.bridge_client.add_record(
-                    "2000-10-10 12:34:56", "holla4adolla", "telegram", words
-                )
-
-    async def process_message_queue(self):
+        logging.debug(f"Check Set: {self.check_set}")
         while True:
             try:
                 msg = await self._queue.get()
@@ -53,27 +35,39 @@ class Worker:
             # TODO Maybe it's faster if we compiled a regex of everything in
             # self.check_set
             words = re.findall(WORD_REGEX, msg.message)
-            # may need to figure things out depending on the timezone
-            # information of the message
-            posted = msg.date.strftime("%Y-%m-%d, %H:%M:%S")
+            out = []
+            if words:
+                logging.info(f"Words: {words}")
+                for word in words:
+                    if word.lower() in self.check_set:
+                        out.append(word.lower())
+                # out = [word.lower() for word in words if word.lower() in self.check_set]
+                logging.info(f"Parsed: {out}")
 
-            # TODO create look ups tasks (perhaps this could be celery beat
-            # server side
-            author = "na"
-            if msg.from_id:
-                author = str(msg.from_id)
+            if out:
+                posted = msg.date.strftime("%Y-%m-%d %H:%M:%S")
 
-            logging.info("Words Parsed: %s" % words)
-            await self.bridge_client.add_record(
-                posted, author, "telegram", words
-            )
+                # TODO create look ups tasks (perhaps this could be celery beat
+                # server side
+                author = "na"
+                if msg.from_id:
+                    author = str(msg.from_id)
+
+                self.bridge_client.add_record(posted, author, "telegram", out)
 
 
 def main():
+    check_set = set()
+    with open("cryptos.txt") as f:
+        for word in f.readlines():
+            check_set.add(word.strip().lower())
+
     loop = asyncio.get_event_loop()
     queue = asyncio.Queue(loop=loop)
 
-    worker = Worker(loop, queue)
+    bc = BridgeClient("bridge")
+
+    worker = Worker(loop, queue, bc, check_set=check_set)
 
     tg_client = TG_Client(loop, queue)
 
